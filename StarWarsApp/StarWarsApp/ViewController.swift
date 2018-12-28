79a31f3f7c5f441bda6438864a1d689733eddd24
David López <quindos00@gmail.com>
2018-12-28 19:19:27 +0100

    Project setup

---
+++ StarWarsApp/StarWarsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsApp
+//
+//  Created by David López on 28/12/18.
+//  Copyright © 2018 David López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
